d7838cfcb8fd0a7e494a3d80f6ba84e7a71e5946
Zachariah Mully <zack.mully@zephyrai.bio>
2022-09-07 08:54:26 -0400

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,16 @@
+import { cdk, javascript } from 'projen';
+const project = new cdk.JsiiProject({
+  author: 'Zachariah Mully',
+  authorAddress: 'zack.mully@zephyrai.bio',
+  defaultReleaseBranch: 'main',
+  name: 'scala-test',
+  packageManager: javascript.NodePackageManager.NPM,
+  projenrcTs: true,
+  repositoryUrl: 'https://github.com/zack.mully/scala-test.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
